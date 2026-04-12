bafdd7e00c4228ece85312dea1e9b9f912297249
Richard <richard.daubner@icloud.com>
2026-04-12 17:20:16 +0200

    Initial commit - CapitalCraft Fabric mod setup

---
+++ src/main/java/com/capitalcraft/capitalcraft/Capitalcraft.java
@@ -0,0 +1,10 @@
+package com.capitalcraft.capitalcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Capitalcraft implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/capitalcraft/capitalcraft/client/CapitalcraftClient.java
@@ -0,0 +1,10 @@
+package com.capitalcraft.capitalcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CapitalcraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
